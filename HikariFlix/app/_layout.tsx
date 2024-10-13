3bdb035a25088ea8bb26782ac0cf077177065d92
geekobueno <geeklove2222@gmail.com>
2024-10-13 18:47:16 +0000

    small header changes

--- HikariFlix/app/_layout.tsx
+++ HikariFlix/app/_layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout() {
           options={{ 
             title: 'HikariFlix',
             headerShown: true,
+            headerTitleAlign: 'center'
           }} 
         />
         <Stack.Screen 
